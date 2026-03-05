5c975c173da1d69561283655726dc8d2d3180734
saiRam <mavillasairam1997@gmail.com>
2026-03-05 23:16:50 +0530

    adding files

---
+++ environments/dev/variables.tf
@@ -0,0 +1,51 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_prefix" {
+  type = list(string)
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "VM size"
+  type        = string
+}
+
+variable "allowed_ip" {
+  description = "Allowed IP for SSH"
+  type        = string
+}
+variable "keyvault_id" {
+  description = "Azure Key Vault ID"
+  type        = string
+}
+variable "admin_username" {
+  description = "VM admin username"
+  type        = string
+}
---
+++ modules/keyvault/variables.tf
@@ -0,0 +1,4 @@
+variable "keyvault_id" {
+  description = "Azure Key Vault ID"
+  type        = string
+}
